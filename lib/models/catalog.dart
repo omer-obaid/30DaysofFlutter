a3b7794d95a66fcd528ac1a5186f9c90d803d065
omerobaid@tds.com.sa <omerobaid@tds.com.sa>
2022-08-22 14:05:51 +0300

    day21 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -6,6 +6,13 @@ class CatalogModel {
     Item(1, "iPhone 12 Pro", "Apple iPhone 12th generation", 999, "#33505a",
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
   ];
+
+  //get item by id
+  static Item getById(int id) =>
+      items.firstWhere((element) => element.id == id, orElse: null);
+
+  //get item by position
+  static Item getByPosition(int pos) => items[pos];
 }
 
 class Item {
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -18,19 +18,22 @@ class MyTheme {
           titleTextStyle: Theme.of(context).textTheme.headline6));
 
   static ThemeData darkTheme(BuildContext context) => ThemeData(
-      brightness: Brightness.dark,
-      primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.poppins().fontFamily,
-      cardColor: Colors.black,
-      canvasColor: darkcreamColor,
-      accentColor: Colors.white,
-      buttonColor: lightBluishColor,
-      appBarTheme: AppBarTheme(
-          centerTitle: true,
-          color: Colors.black,
-          elevation: 0.0,
-          iconTheme: IconThemeData(color: Colors.white),
-          titleTextStyle: Theme.of(context).textTheme.headline6));
+        brightness: Brightness.dark,
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        cardColor: Colors.black,
+        canvasColor: darkcreamColor,
+        accentColor: Colors.white,
+        buttonColor: lightBluishColor,
+        appBarTheme: AppBarTheme(
+            centerTitle: true,
+            color: Colors.black,
+            elevation: 0.0,
+            iconTheme: IconThemeData(color: Colors.white),
+            textTheme: Theme.of(context).textTheme.copyWith(
+                headline6: context.textTheme.headline6!
+                    .copyWith(color: Colors.white))),
+      );
 
   static Color creamColor = Color(0xfff5f5f5);
   static Color darkcreamColor = Vx.gray900;
